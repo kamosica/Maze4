21d2484400455df79decd4bf9aedd0a83114898c
kamosica <nkc.kenta.oishi@gmail.com>
2018-01-24 15:04:43 +0900

    プレイヤーを光らせる

---
+++ Assets/Maze/PlayerDamage.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDamage : MonoBehaviour {
+
+    Status status_scr;
+
+	// Use this for initialization
+	void Start () {
+        status_scr = gameObject.GetComponent<Status>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Enemy")
+        {
+            status_scr.Damages(1.0f);
+        }
+    }
+}
